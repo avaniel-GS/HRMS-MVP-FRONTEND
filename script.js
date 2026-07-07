90e2c289baa78a7392e128091d0e8d11bbd87bbb
thomas-jino <contact.thomasjino@gmail.com>
2026-07-07 23:17:56 +0530

    Added a sample add-employee-popup

---
+++ script.js
@@ -0,0 +1,5 @@
+const employee_add_btn = document.getElementById("employee_add");
+const add_employee_form = document.getElementById("add_employee_form");
+employee_add_btn.addEventListener("click", function() {
+    add_employee_form.style.display = "flex";
+})
